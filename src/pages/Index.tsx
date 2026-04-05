7ed74021500a824a3c95179a021cfa38b4c54a49
FainowAI <fainowadm@gmail.com>
2026-04-05 13:09:43 -0300

    Tirando sessão de ultimo post da página

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,18 +1,12 @@
 import Navbar from "@/components/Navbar";
 import FullscreenHero from "@/components/FullscreenHero";
-import HeroPost from "@/components/HeroPost";
 import AboutDrica from "@/components/AboutDrica";
 import LatestPosts from "@/components/LatestPosts";
 import BrowseThemes from "@/components/BrowseThemes";
 import Newsletter from "@/components/Newsletter";
 import Footer from "@/components/Footer";
 
-import { HeroPostSkeleton } from "@/components/skeletons";
-import { useFeaturedPost } from "@/hooks/usePosts";
-
 const Index = () => {
-  const { data: featuredPost, isLoading: isLoadingFeatured } = useFeaturedPost();
-
   return (
     <div className="min-h-screen">
       <Navbar />
@@ -20,30 +14,6 @@ const Index = () => {
       <FullscreenHero />
 
       <AboutDrica />
-
-      {isLoadingFeatured ? (
-        <section className="py-12 md:py-20 lg:py-24 bg-background">
-          <div className="container mx-auto px-4">
-            <HeroPostSkeleton />
-          </div>
-        </section>
-      ) : featuredPost ? (
-        <HeroPost
-          category={featuredPost.categories?.name?.toUpperCase() || ""}
-          title={featuredPost.title}
-          summary={featuredPost.summary || ""}
-          image={featuredPost.featured_image || "/placeholder.svg"}
-          link={`/artigo/${featuredPost.slug}`}
-        />
-      ) : (
-        <HeroPost
-          category="BLOG"
-          title="Bem-vinda ao Drica Divina"
-          summary="Descubra conteúdos sobre Skincare, Cabelo, Maquiagem e Saúde."
-          image="/placeholder.svg"
-          link="/blog"
-        />
-      )}
 
       <LatestPosts />
       <BrowseThemes />
